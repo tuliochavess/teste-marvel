69889c34582c4b98c3daaf94511a381418856bb1
Túlio Chaves <tuliochavess@gmail.com>
2021-09-25 15:07:58 -0300

    Add files via upload

--- src/Components/Personagens.js
+++ src/Components/Personagens.js
@@ -5,7 +5,7 @@ import stars from "../Assets/Personagens/stars.svg";
 import close from "../Assets/Personagens/close.svg";
 
 const Personagens = () => {
-  let [show, setShow] = React.useState('main');
+  let [show, setShow] = React.useState("main");
   console.log(show);
   return (
     <>
@@ -14,7 +14,9 @@ const Personagens = () => {
         {/* -------------------------------------- Início box principal 1 -------------------------------------- */}
         <div
           className={`${styles.mainBox1} ${
-            show === 'wanda' || show === 'thanos' || show === 'hulk' ? styles.dontShownOnScreen : ''
+            show === "wanda" || show === "thanos"
+              ? styles.dontShownOnScreen
+              : ""
           }`}
         >
           <div className={styles.backgroundMainImg}>
@@ -27,7 +29,7 @@ const Personagens = () => {
             </p>
             <span
               onClick={() => {
-                setShow((show = 'wanda'));
+                setShow((show = "wanda"));
               }}
             >
               ver detalhes
@@ -39,7 +41,7 @@ const Personagens = () => {
         {/* -------------------------------------- Início box detalhes 1 -------------------------------------- */}
         <div
           className={`${styles.mainBox1Details} ${
-            show === 'wanda' ? "" : styles.dontShownOnScreen
+            show === "wanda" ? "" : styles.dontShownOnScreen
           }`}
         >
           <div className={styles.details}>
@@ -61,7 +63,7 @@ const Personagens = () => {
               alt="Ícone fechar Modal"
               className={styles.close}
               onClick={() => {
-                setShow((show = 'main'));
+                setShow((show = "main"));
               }}
             />
           </div>
@@ -71,7 +73,9 @@ const Personagens = () => {
         {/* -------------------------------------- Início box principal 2 -------------------------------------- */}
         <div
           className={`${styles.mainBox2} ${
-            show === 'wanda' || show === 'thanos' || show === 'hulk' ? styles.dontShownOnScreen : ""
+            show === "wanda" || show === "thanos" || show === "hulk"
+              ? styles.dontShownOnScreen
+              : ""
           }`}
         >
           <div className={styles.backgroundMainImg}>
@@ -84,7 +88,7 @@ const Personagens = () => {
             </p>
             <span
               onClick={() => {
-                setShow((show = 'thanos'));
+                setShow((show = "thanos"));
               }}
             >
               ver detalhes
@@ -92,6 +96,95 @@ const Personagens = () => {
           </div>
         </div>
         {/* -------------------------------------- Final box principal 2 -------------------------------------- */}
+
+        {/* -------------------------------------- Início box detalhes 2 -------------------------------------- */}
+        <div
+          className={`${styles.mainBox2Details} ${
+            show === "thanos" ? "" : styles.dontShownOnScreen
+          }`}
+        >
+          <div className={styles.details}>
+            <h2>Thanos</h2>
+            <p className={styles.aparicoes}>Aparições:</p>
+            <p>Vingadores</p>
+            <p>Guardiões da Galáxia - vol. I</p>
+            <p>Vingadores - Guerra Infinita</p>
+            <p>Vingadores - Ultimato</p>
+            <p>.</p>
+            <h3>Avaliações dos Fãs</h3>
+            <img
+              src={stars}
+              alt="Estrelas Avaliação"
+              className={styles.stars}
+            />
+            <img
+              src={close}
+              alt="Ícone fechar Modal"
+              className={styles.close}
+              onClick={() => {
+                setShow((show = "main"));
+              }}
+            />
+          </div>
+        </div>
+        {/* -------------------------------------- Final box detalhes 2 -------------------------------------- */}
+
+        {/* -------------------------------------- Início box principal 3 -------------------------------------- */}
+        <div
+          className={`${styles.mainBox3} ${
+            show === "hulk" ? styles.dontShownOnScreen : ""
+          }`}
+        >
+          <div className={styles.backgroundMainImg}>
+            <h2>Hulk</h2>
+            <p>
+              Na história original dos quadrinhos, o Hulk é um selvagem e
+              poderoso alter ego do Dr. Robert Bruce Banner, um cientista que
+              foi atingido por raios gama enquanto salvava um adolescente
+              durante o teste militar.
+            </p>
+            <span
+              onClick={() => {
+                setShow((show = "hulk"));
+              }}
+            >
+              ver detalhes
+            </span>
+          </div>
+        </div>
+        {/* -------------------------------------- Final box principal 3 -------------------------------------- */}
+
+        {/* -------------------------------------- Início box detalhes 3 -------------------------------------- */}
+        <div
+          className={`${styles.mainBox2Details} ${
+            show === "hulk" ? "" : styles.dontShownOnScreen
+          }`}
+        >
+          <div className={styles.details}>
+            <h2>Hulk</h2>
+            <p className={styles.aparicoes}>Aparições:</p>
+            <p>Vingadores</p>
+            <p>Vingadores - Era de Ultron</p>
+            <p>Thor - Ragnarok</p>
+            <p>Vingadores - Guerra Infinita</p>
+            <p>Vingadores - Ultimato</p>
+            <h3>Avaliações dos Fãs</h3>
+            <img
+              src={stars}
+              alt="Estrelas Avaliação"
+              className={styles.stars}
+            />
+            <img
+              src={close}
+              alt="Ícone fechar Modal"
+              className={styles.close}
+              onClick={() => {
+                setShow((show = "main"));
+              }}
+            />
+          </div>
+        </div>
+        {/* -------------------------------------- Final box detalhes 3 -------------------------------------- */}
       </section>
     </>
   );
